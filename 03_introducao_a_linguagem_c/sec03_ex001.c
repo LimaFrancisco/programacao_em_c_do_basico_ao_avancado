2ed5a92674ef6ec65c74f77a98c294612d16f55a
FranciscoLima <francisco1111.fl@protonmail.com>
2023-03-05 10:28:01 -0500

    primeiro exercicio da secao 3

---
+++ 03_introducao_a_linguagem_c/sec03_ex001.c
@@ -0,0 +1,15 @@
+//O programa deve podir um numero inteiro do usuario e retornar esse valor na tela
+
+#include <stdio.h>
+
+int main(void){
+
+    int num = 0;
+
+    printf("Informe um numero inteiro qualquer: \n");
+    scanf("%d", &num);
+
+    printf("O numero inteiro informado eh: %d\n", num);
+
+    return 0;
+}
